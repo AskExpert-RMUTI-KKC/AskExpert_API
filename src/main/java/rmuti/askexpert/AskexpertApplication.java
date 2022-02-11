94545385c6e78ee21d777282e2421e44bf79cc45
thanapun.th <thanapun.th@rmuti.ac.th>
2022-02-11 07:59:56 +0700

    everytime run main view create MKDIR

--- src/main/java/rmuti/askexpert/AskexpertApplication.java
+++ src/main/java/rmuti/askexpert/AskexpertApplication.java
@@ -3,10 +3,17 @@ package rmuti.askexpert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class AskexpertApplication {
 
 	public static void main(String[] args) {
+		//create Folder
+		new File("uploads").mkdir();
+		new File("uploads/profileImg").mkdir();
+		new File("uploads/topicImg").mkdir();
+		new File("uploads/commentImg").mkdir();
 		SpringApplication.run(AskexpertApplication.class, args);
 	}
 
