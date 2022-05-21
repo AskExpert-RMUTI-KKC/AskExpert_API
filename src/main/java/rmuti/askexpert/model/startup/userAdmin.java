ae913c166f2dad31b751f0d8d256053295028e4f
NamelessKingMeow <ubolchai2000@gmail.com>
2022-05-21 11:25:42 +0700

    add Admin with StartUp processs

---
+++ src/main/java/rmuti/askexpert/model/startup/userAdmin.java
@@ -0,0 +1,37 @@
+package rmuti.askexpert.model.startup;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import rmuti.askexpert.model.repo.UserNameRepository;
+import rmuti.askexpert.model.table.UserName;
+
+import java.util.Optional;
+
+@Component
+public class userAdmin {
+    @Autowired
+    private UserNameRepository userNameRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @EventListener
+    public void addAdmin(ApplicationEvent event){
+        UserName admin = new UserName();
+
+        if(!userNameRepository.existsByEmail("Admin")){
+            admin.setEmail("Admin");
+            admin.setPassWordFb("0");
+            admin.setPassWordGoogle("0");
+            admin.setPassWord(passwordEncoder.encode("Admin"));
+            admin.setRole("ADMIN");
+            userNameRepository.save(admin);
+        }
+
+
+    }
+}
